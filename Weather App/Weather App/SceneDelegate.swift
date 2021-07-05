380bd92abeee40307b0b7bdcf5577bb7812368fd
goallath <kozikarthur@gmail.com>
2021-07-06 02:04:25 +0300

    Create TabBarController with main screen.

--- Weather App/Weather App/SceneDelegate.swift
+++ Weather App/Weather App/SceneDelegate.swift
@@ -16,7 +16,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        let viewController = MainWeatherViewController.instantiate()
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "location"), selectedImage: UIImage(systemName: "location"))
+        let tabBarController = TabBarController.instantiate()
+        tabBarController.viewControllers = [navController]
+        tabBarController.selectedIndex = 0
+        
+        
+        window.rootViewController = tabBarController
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
