130aa9a615d32031edd27570adae4705bc907421
goallath <kozikarthur@gmail.com>
2021-07-08 01:17:16 +0300

    Test.

--- Weather App/Weather App/MainWeatherViewController.swift
+++ Weather App/Weather App/MainWeatherViewController.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class MainWeatherViewController: UIViewController, Storyboarded {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        taskOnePointOne()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func taskOnePointOne() {
+        print(50)
     }
-    */
-
+    
 }
